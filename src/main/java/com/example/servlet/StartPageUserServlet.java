d31670723492de3fc03c017cf4f5d4c6e1183a4b
Yuliya Raikova <yuradev2001@gmail.com>
2023-01-01 14:07:33 +0300

    seems to work, finally(2)

--- src/main/java/com/example/servlet/StartPageUserServlet.java
+++ src/main/java/com/example/servlet/StartPageUserServlet.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("")
+@WebServlet("/index")
 public class StartPageUserServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
